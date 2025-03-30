8c322c97a956cae0ef860f88d074ffa256539c63
Krushovice <kickstar69@yandex.ru>
2025-03-30 16:33:26 +1000

    create fastapi app

---
+++ webhook_service/app.py
@@ -0,0 +1,34 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI, Request
+
+from trading_bot.trade_logic import Bot
+from utils.logger import setup_logger
+from utils.storage import PositionStorage
+
+logger = setup_logger(__name__)
+
+
+@asynccontextmanager
+async def lifespan(_):
+    bot.verify_position_with_exchange()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+storage = PositionStorage()
+bot = Bot(storage=storage)
+
+
+@app.post("/trw_webhook")
+async def webhook(request: Request):
+    data = await request.json()
+    side = data["side"]
+    symbol = data["symbol"]
+    limit_price = float(data["limit_price"])
+    qty = float(data["qty"])
+
+    logger.info(f"Webhook: {side} {symbol} qty: {qty} at price: {limit_price}")
+    res = bot.execute_trade(side, qty, limit_price)
+
+    return {"status": "ok"}
