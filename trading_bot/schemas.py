6cf7462ba537e500face47acb24a22f0b418d0f9
Krushovice <kickstar69@yandex.ru>
2025-06-04 23:28:02 +1000

    update schema with stricter params

--- trading_bot/schemas.py
+++ trading_bot/schemas.py
@@ -1,15 +1,17 @@
-# schemas.py
 from datetime import datetime
+from typing import Annotated, Literal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class TradingViewSignal(BaseModel):
     secret: str
     symbol: str
-    side: str
-    qty: float
-    price: float
+    side: Literal["buy", "sell"]
+    qty: Annotated[float, Field(ge=0.01, lt=1.0)]
+    price: Annotated[float, Field(ge=1.0)]
     trigger_time: datetime
     max_lag: int
     strategy_id: str
+
+    model_config = {"extra": "forbid"}
