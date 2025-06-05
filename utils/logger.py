c91fb344d145ad9fa0774fdb8b61474e3bfed8c2
Krushovice <kickstar69@yandex.ru>
2025-06-05 19:30:27 +1000

    update logger

--- utils/logger.py
+++ utils/logger.py
@@ -1,30 +1,39 @@
+from datetime import datetime
 import logging
 from logging.handlers import RotatingFileHandler
 import os
+from pathlib import Path
 import sys
 
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = Path(__file__).resolve().parent
+LOG_DIR = os.path.join(BASE_DIR, "logs")
 
 
-def setup_logger(module_name):
+def delete_old_logs(log_dir: str):
+    for file in os.listdir(log_dir):
+        if file.endswith(".log"):
+            os.remove(os.path.join(log_dir, file))
+
+
+def setup_logger(module_name: str) -> logging.Logger:
     logger = logging.getLogger(module_name)
-    logger.setLevel(
-        logging.DEBUG
-    )  # Общий уровень для логгера (управляется handler'ами отдельно)
+    logger.setLevel(logging.DEBUG)
 
-    filename = os.path.splitext(os.path.basename(module_name))[0]
+    today_str = datetime.now().strftime("%Y-%m-%d")
+    log_filename = (
+        f"{os.path.splitext(os.path.basename(module_name))[0]}_{today_str}.log"
+    )
+    log_path = os.path.join(LOG_DIR, log_filename)
 
     if not logger.handlers:
-        # Создание директории для логов, если её нет
-        log_dir = f"{BASE_DIR}/logs"
-        os.makedirs(log_dir, exist_ok=True)
+        os.makedirs(LOG_DIR, exist_ok=True)
+        delete_old_logs(LOG_DIR)
 
-        # Файловый обработчик (только ошибки и выше)
         file_handler = RotatingFileHandler(
-            f"{log_dir}/{filename}.log",
-            maxBytes=100000,
-            backupCount=5,
+            log_path,
+            maxBytes=100_000,
+            backupCount=1,
             encoding="utf-8",
         )
         file_handler.setLevel(logging.ERROR)
@@ -34,7 +43,6 @@ def setup_logger(module_name):
         file_handler.setFormatter(file_formatter)
         logger.addHandler(file_handler)
 
-        # Консольный обработчик (только INFO)
         console_handler = logging.StreamHandler(sys.stdout)
         console_handler.setLevel(logging.INFO)
         console_formatter = logging.Formatter("%(asctime)s - %(message)s")
